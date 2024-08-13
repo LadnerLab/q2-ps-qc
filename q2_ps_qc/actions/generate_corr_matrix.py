61c6432da00ff4b41e1ab5ea2450c12a5dfd1c72
eelko09 <55642194+eelko09@users.noreply.github.com>
2024-08-13 08:41:12 -0700

    Update generate_corr_matrix.py
    Testing new output correlation tsv file

--- q2_ps_qc/actions/generate_corr_matrix.py
+++ q2_ps_qc/actions/generate_corr_matrix.py
@@ -204,6 +204,7 @@ def generate_corr_matrix(
                 # Create a data frame & convert it into a correlation matrix
                 data_frame = pd.DataFrame(data=replicate_pair_dict)
                 corr_matrix = [data_frame.corr(method='pearson')]
+                np.savetxt("output.tsv", corr_matrix, delimiter="\t")
 
                 for matrix in corr_matrix:
                     score_found = False
